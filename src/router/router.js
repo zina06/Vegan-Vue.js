dedf73617cde9c848f4cd14ee30401f82a493b38
jina <dhaud42@naver.com>
2023-04-10 11:52:15 +0900

    라우터 추가

--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Main from '@/pages/Main.vue';
-import Join from '@/pages/member/join.vue';
+import Signup from '@/pages/member/signup.vue';
 import Detail from '@/pages/restaurant/detail.vue';
 import Search from '@/pages/restaurant/search.vue';
+import Login from '@/pages/member/login.vue';
+import Mydining from '@/pages/member/my-dining.vue';
+import Mypage from '@/pages/member/mypage.vue';
+import Reviewinsert from '@/pages/member/review-insert.vue';
+import Owner from '@/pages/owner/manage.vue';
+import Reserve from '@/pages/reservation/reserve.vue';
+import Confirm from '@/pages/reservation/confirm.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -17,15 +25,51 @@ const router = createRouter({
             component: Detail    
         },
         {
-            path: "/member/join",
-            name: "Join",
-            component: Join    
+            path: "/member/signup",
+            name: "Signup",
+            component: Signup    
         },
         {
             path: "/restaurant/search",
             name: "Search",
             component: Search    
         },
+        {
+            path: "/member/my-dining",
+            name: "Mydining",
+            component: Mydining    
+        },
+        {
+            path: "/member/login",
+            name: "Login",
+            component: Login    
+        },
+        {
+            path: "/member/mypage",
+            name: "Mypage",
+            component: Mypage    
+        },
+        {
+            path: "/member/reviewinsert",
+            name: "Reviewinsert",
+            component: Reviewinsert    
+        },
+        {
+            path: "/manage",
+            name: "Owner",
+            component: Owner    
+        },
+        {
+            path: "/reserve",
+            name: "Reserve",
+            component: Reserve    
+        },
+        {
+            path: "/reserve/confirm",
+            name: "Confirm",
+            component: Confirm    
+        },
+
         
     ]
 });
