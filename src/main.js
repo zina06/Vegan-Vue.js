0a5fc336d11c10b0a856722c72691871ff4a38e4
choi-sunbin <csn1434@naver.com>
2023-04-12 10:25:57 +0900

    search

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/router";
+import { createNaverMap } from "vue3-naver-maps";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(createNaverMap, {
+    clientId: "w7yy7bbaa1", // Required
+    category: "ncp", // Optional
+    subModules: [], // Optional, "panorama" | "geocoder" | "drawing" | "visualization"
+  })
+  .mount("#app");
